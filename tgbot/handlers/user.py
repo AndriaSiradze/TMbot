9088c119472311c158c2f20e2f3ad1aa5860ce31
Andria Siradze <andreisiradze@gmail.com>
2026-01-31 17:35:32 +0400

    user exists checking

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -1,3 +1,5 @@
+import logging
+
 from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
@@ -15,8 +17,12 @@ async def admin_start(message: Message, dialog_manager:DialogManager,config:Conf
     agcm = await config.gspread_conf.agcm.authorize()
     sheet = await agcm.open('TMsheet')
     wk: AsyncioGspreadWorksheet = await sheet.get_worksheet(0)
-    # data = await wk.col_values()
-
-    # all_users = awa
+    data = await wk.col_values(8)
+    if f'{message.from_user.id}' in data:
+        await message.answer('вы уже записаны на обучение')
+        return
+    logging.info(message.from_user.id)
+    logging.info(data)
+    logging.info(message.from_user.id in data)
     await dialog_manager.start(SelectStudies.select_city)
 
